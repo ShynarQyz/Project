26e3a505d7555f7fac72921ae4871dfdd5cf58d9
NAME <shynarmyrzan@gmail.com>
2024-02-01 14:28:03 +0600

    first commit

---
+++ types.js
@@ -0,0 +1,24 @@
+// Типы данных
+let str = 'Школа программирования Decode'
+let str1 = "Школа программирования Decode"
+let str2 = `Школа программирования Decode`
+console.log(str);
+console.log(str1);
+console.log(str2);
+
+let txt = 'Школа\nпрограммирования\nDecode'
+let txt1 = `Школа 
+программирования
+Decode`
+console.log(txt);
+console.log(txt1);
+
+let a = 'Hello'
+let b = 'Decode'
+let c = '!'
+console.log(a + b + c);
+console.log(`${a} , ${b} ${c} Добро пожаловать на наши курсы`);
+
+let bool = true // false
+let bool1 = 1 > 5;
+console.log(bool1);
